475dcede3eeb08699b276ef1224bdf9a3df3165c
Orçun Aşık <asikorcun@gmail.com>
2023-11-02 00:34:20 +0300

    Configuration and Services have been reported to the IoC for DI.

--- FrontEnd/SalesAndStockAutomation.BlazorUI/Program.cs
+++ FrontEnd/SalesAndStockAutomation.BlazorUI/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 using SalesAndStockAutomation.BlazorUI;
+using SalesAndStockAutomation.Configurations;
+using SalesAndStockAutomation.Services.Abstracts;
+using SalesAndStockAutomation.Services.Concretes;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +14,22 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 #region Components
 builder.Services.AddRadzenComponents();
 builder.Services.AddBlazoredToast();
+#endregion
+
+#region Configurations
+builder.Services.AddScoped<IDomainService, DomainManager>();
+#endregion
+
+#region Services
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IAuthorityService, AuthorityService>();
+builder.Services.AddScoped<ICityService, CityService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ISaleService, SaleService>();
+builder.Services.AddScoped<IStockService, StockService>();
+builder.Services.AddScoped<IUserAuthorityService, UserAuthorityService>();
 #endregion
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
